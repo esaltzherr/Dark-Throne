03a33814a3e122d26cfe297142901114af52ba51
Elroy Saltzherr <lsaltzherr@gmail.com>
2024-02-01 16:56:26 -0800

    added some simples

---
+++ Dark Throne/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool canJump = true;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vecotor2(dirX, rb.velocity.y)
+        if(Input.GetKeyDown("space")){
+            if(canJump){
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 14f, 0);
+            }
+            
+        }
+    }
+}
