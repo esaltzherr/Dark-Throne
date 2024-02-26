2154de9bf771908812f333f8f51b1b40ec9e1ce3
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2024-02-26 14:15:02 -0800

    EnemyHealth and PlayerInvulnerability scripts. Enemy Health added to enemy sprite.

---
+++ Dark Throne/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
+
