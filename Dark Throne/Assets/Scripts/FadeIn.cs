96aa2d35ca5c04ce2cbace85294bfda9254bc726
JoseOrantesL <jmorante@ucsc.edu>
2024-02-28 20:21:43 -0800

    Added fade in, and health var

---
+++ Dark Throne/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1f; 
+
+    void Start()
+    {
+        // Start the fade-in effect
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+
+        fadeImage.color = new Color(0f, 0f, 0f, 1f);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            fadeImage.color = new Color(0f, 0f, 0f, alpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeImage.color = new Color(0f, 0f, 0f, 0f);
+    }
+}
