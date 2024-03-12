286b7c913b2580d8f4c3816e3a3cfa4c9c0083f2
Elroy Saltzherr <lsaltzherr@gmail.com>
2024-03-12 15:36:17 -0700

    cleaned up function, and deleted character for level 2

--- Dark Throne/Assets/Scripts/LevelMove_Ref.cs
+++ Dark Throne/Assets/Scripts/LevelMove_Ref.cs
@@ -5,24 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelMove_Ref : MonoBehaviour
 {
-    public int sceneBuildIndex;
-    public bool playerInDoor = false;
+    
+    bool playerInDoor = false;
     public string nextScene;
-
-    // Update is called once per frame
-    // private void OnTriggerEnter2D(Collider2D other){
-    //     print("Trigger Entered");
-        
-    //     if(other.tag == "Player"){
-    //         print("Switching Scene to " + sceneBuildIndex);
-    //         SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-    //    } 
-    // }
-
-    // public void update(){
-
       
-
     void Update()
     {
 
