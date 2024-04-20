b92ad415998a78c8292fc7b626ad9cf640fa08a7
iceypotato <nickyjedi@gmail.com>
2024-04-19 22:41:17 -0700

    Added minecraft parallax background for cavelevel1

---
+++ Dark Throne/Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public GameObject _camera;
+    private float _length, _startPos;
+    
+    public float paralaxAmount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.position.x;
+        _length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (_camera.transform.position.x * paralaxAmount);
+
+        transform.position = new Vector3(_startPos + dist, transform.position.y, transform.position.x);
+    }
+}
