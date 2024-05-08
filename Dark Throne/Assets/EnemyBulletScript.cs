74a1f6e2ea38250c31cad796ee42cfb0790a764d
486ben <zhuan152@ucsc.edu>
2024-05-07 23:48:16 -0700

    update for the game and health system.

---
+++ Dark Throne/Assets/EnemyBulletScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+
+    private GameObject player;
+    private Rigidbody2D rb;
+
+    public float force;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+       player = GameObject.FindGameObjectWithTag("Player");
+
+       Vector3 direction = player.transform.position - transform.position;
+       rb.velocity = new Vector2(direction.x, direction.y).normalized * force; 
+
+       float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+       transform.rotation = Quaternion.Euler(0,0,rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 5)
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealth2>().TakeDamage(10);
+            //Destroy(gameObject);
+        }
+    }
+
+    // void OnTriggerEnter2D(Collision2D other)
+    // {
+    //     if(other.gameObject.CompareTag("Player"))
+    //     {
+    //         other.gameObject.GetComponent<PlayerHealth2>().TakeDamage(10);
+    //         Destroy(gameObject);
+    //     }
+    // }
+}
--- Dark Throne/Assets/EnemyShooting.cs
+++ Dark Throne/Assets/EnemyShooting.cs
@@ -8,21 +8,28 @@ public class EnemyShooting : MonoBehaviour
     public Transform bulletPos;
 
     private float timer;
+    private GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        float distance = Vector2.Distance(transform.position,player.transform.position);
+        //Debug.Log(distance);
 
-        if(timer > 2){
-            timer = 0;
-            shoot();
-
+        if(distance < 10)
+        {
+            timer += Time.deltaTime;
+            if(timer > 2)
+            {
+                timer = 0;
+                shoot();
+            }
         }
     }
 
