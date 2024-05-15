72a1b54eba54ebf0a11ef52199e96e2b9b79a2fa
iceypotato <nickyjedi@gmail.com>
2024-05-14 21:42:45 -0700

    pushed mainmenubutton script and gong sfx

---
+++ Dark Throne/Assets/Scripts/Menus/MainMenuButton.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private float _originalPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _originalPos = gameObject.GetComponent<RectTransform>().position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveRight();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MoveLeft();
+    }
+
+    public void MoveRight()
+    {
+        var rectTransform = this.gameObject.GetComponent<RectTransform>();
+        var currentX = rectTransform.position.x;
+        rectTransform.DOMoveX(_originalPos + 5, 1);
+    }
+
+    public void MoveLeft()
+    {
+        var rectTransform = this.gameObject.GetComponent<RectTransform>();
+        var currentX = rectTransform.position.x;
+        rectTransform.DOMoveX(_originalPos, 1);
+    }
+}
