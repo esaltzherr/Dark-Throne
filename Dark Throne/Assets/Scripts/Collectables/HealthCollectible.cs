5b857d706a31ebaa8507a96452dd2ea0ec427a9c
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2024-05-23 20:03:17 -0700

    Player collectable items

--- Dark Throne/Assets/Scripts/Collectables/HealthCollectible.cs
+++ Dark Throne/Assets/Scripts/Collectables/HealthCollectible.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    // [SerializeField]
-    // private int healthBonus = 10; // value of health increase
+    [SerializeField]
+    private int healthBonus = 1; // value of health increase
 
-    // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     if (collision.gameObject.tag == "Player")
-    //     {
-    //         PlayerHealth2 playerHealth = collision.gameObject.GetComponent<PlayerHealth2>();
-    //         if (playerHealth != null)
-    //         {
-    //             playerHealth.CollectHealthItem(healthBonus); // manage health item collection
-    //             Destroy(gameObject); // destroy collectable
-    //         }
-    //     }
-    // }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerHealth2 maxHearts = collision.gameObject.GetComponent<PlayerHealth2>();
+            if (maxHearts != null)
+            {
+                maxHearts.CollectHealthItem(healthBonus); // manage health item collection
+                Destroy(gameObject); // destroy collectable
+            }
+        }
+    }
 }
