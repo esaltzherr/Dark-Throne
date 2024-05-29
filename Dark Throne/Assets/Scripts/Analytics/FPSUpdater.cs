a1ebb97d3d8ce26739382a3bddd08c511eb9b1e7
Elroy Saltzherr <lsaltzherr@gmail.com>
2024-05-29 14:44:09 -0700

    dont destroy

--- Dark Throne/Assets/Scripts/Analytics/FPSUpdater.cs
+++ Dark Throne/Assets/Scripts/Analytics/FPSUpdater.cs
@@ -3,17 +3,20 @@ using TMPro;
 
 
 
-public class FPSUpdater : MonoBehaviour 
+public class FPSUpdater : MonoBehaviour
 {
     float fps;
     [SerializeField] float updateFrequency = 0.2f;
     float updateTimer;
+    private static FPSUpdater instance;
+
 
     [SerializeField] TextMeshProUGUI fpsTitle;
     private void UpdateFPSDisplay()
     {
         updateTimer -= Time.deltaTime;
-        if (updateTimer <= 0f){
+        if (updateTimer <= 0f)
+        {
             fps = 1f / Time.unscaledDeltaTime;
             fpsTitle.text = "FPS: " + Mathf.Round(fps);
             updateTimer = updateFrequency;
@@ -25,7 +28,17 @@ public class FPSUpdater : MonoBehaviour
     {
         UpdateFPSDisplay();
     }
-    void Start(){
-        updateTimer = updateFrequency;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.root.gameObject);
+            updateTimer = updateFrequency;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
