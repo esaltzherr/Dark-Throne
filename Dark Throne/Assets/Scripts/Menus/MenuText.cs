ce83eb0a000983e12a67b03e8687ebb80a3ac6bf
iceypotato <nickyjedi@gmail.com>
2024-06-04 19:44:53 -0700

    Fixed up localizationevent thing

--- Dark Throne/Assets/Scripts/Menus/MenuText.cs
+++ Dark Throne/Assets/Scripts/Menus/MenuText.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Settings;
+
 
 public class MenuText : MonoBehaviour
 {
     public LocalizeStringEvent localizeStringEvent;
+    public TMP_FontAsset[] tmpFontAssets;
     
     // Start is called before the first frame update
     void Start()
     {
         // todo: make it so the font changes when a localization event occurs.
-        localizeStringEvent.OnUpdateString.AddListener(OnStringChanged);
+        // localizeStringEvent.OnUpdateString.AddListener(OnStringChanged);
     }
 
     // Update is called once per frame
@@ -23,6 +27,15 @@ public class MenuText : MonoBehaviour
 
     void OnStringChanged(string s)
     {
-        
+        var tmp = gameObject.GetComponent<TextMeshProUGUI>();
+        Debug.Log(PlayerPrefs.GetInt("Lang"));
+        tmp.font = tmpFontAssets[LanguageChanger.CurrentLang];
+    }
+
+    public void UpdateFont()
+    {
+        var tmp = gameObject.GetComponent<TextMeshProUGUI>();
+        // Debug.Log(PlayerPrefs.GetInt("Lang"));
+        tmp.font = tmpFontAssets[LanguageChanger.CurrentLang];
     }
 }
--- Dark Throne/Assets/Scripts/Settings/LanguageChanger.cs
+++ Dark Throne/Assets/Scripts/Settings/LanguageChanger.cs
@@ -5,6 +5,8 @@ using UnityEngine.Localization.Settings;
 
 public class LanguageChanger : MonoBehaviour
 {
+    public static int CurrentLang;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,15 +14,17 @@ public class LanguageChanger : MonoBehaviour
     }
 
     private void Delay(){
-
-        int number = PlayerPrefs.GetInt("Lang");
-        ChangLang(number);
+        //
+        // int number = PlayerPrefs.GetInt("Lang");
+        // ChangLang(number);
     }
 
     // Update is called once per frame
     public void ChangLang(int lang)
     {
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[lang];
-        PlayerPrefs.SetInt("Lang", lang);
+        CurrentLang = lang;
+        // PlayerPrefs.SetInt("Lang", lang);
+        // Debug.Log("changlang" + PlayerPrefs.GetInt("Lang"));
     }
 }
