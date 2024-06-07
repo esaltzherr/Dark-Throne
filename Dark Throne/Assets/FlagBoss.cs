7714eeb9b66a32b35851a52401549e0f1ded5b55
JoseOrantesL <jmorante@ucsc.edu>
2024-06-06 20:48:00 -0700

    Update FlagBoss

--- Dark Throne/Assets/FlagBoss.cs
+++ Dark Throne/Assets/FlagBoss.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class FlagBoss : MonoBehaviour
 {
     public GameObject Tilemap;
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Tilemap.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if(enemy1 == null && enemy2 == null && enemy3 == null)
+        {
+            Tilemap.SetActive(false);
         }
     }
 }
