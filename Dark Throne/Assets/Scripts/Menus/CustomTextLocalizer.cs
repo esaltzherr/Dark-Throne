96a6ec1623aeffaa042eda12fa513b2a2a41fa9a
iceypotato <nickyjedi@gmail.com>
2024-06-13 11:43:36 -0700

    Updated the start game text to reflect whether a player has a save

--- Dark Throne/Assets/Scripts/Menus/CustomTextLocalizer.cs
+++ Dark Throne/Assets/Scripts/Menus/CustomTextLocalizer.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Localization;
 using UnityEngine;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Components;
 using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
+using UnityEngine.Serialization;
 
 
 public class CustomTextLocalizer : MonoBehaviour
 {
-    public LocalizeStringEvent localizeStringEvent;
-    public TMP_FontAsset[] tmpFontAssets;
+    public SaveLoadJSONPlayer playerJSONData;
     
+    public LocalizeStringEvent localizeStringEvent;
+
+    // public TableReference tableReference;
+    public LocalizedString startGameStrID;  // Text that contains the start game text
+    public LocalizedString loadGameStrID;  // text that contains load game text
+
     // Start is called before the first frame update
     void Start()
     {
         // todo: make it so the font changes when a localization event occurs.
-        localizeStringEvent.OnUpdateString.AddListener(OnStringChanged);
+        // localizeStringEvent.OnUpdateString.AddListener(OnStringChanged);
+        OnStringChanged("a");
     }
 
     // Update is called once per frame
@@ -27,15 +36,19 @@ public class CustomTextLocalizer : MonoBehaviour
 
     void OnStringChanged(string s)
     {
-        var tmp = gameObject.GetComponent<TextMeshProUGUI>();
-        Debug.Log(PlayerPrefs.GetInt("Lang"));
-        tmp.font = tmpFontAssets[LanguageChanger.CurrentLang];
-    }
-
-    public void UpdateFont()
-    {
-        var tmp = gameObject.GetComponent<TextMeshProUGUI>();
-        // Debug.Log(PlayerPrefs.GetInt("Lang"));
-        tmp.font = tmpFontAssets[LanguageChanger.CurrentLang];
+        // Debug.Log("current lang: " + LocalizationSettings.SelectedLocale.name);
+        // Debug.Log("current lang: " + LocalizationSettings.SelectedLocale.LocaleName);
+        // Debug.Log("current lang: " + LocalizationSettings.SelectedLocale.SortOrder);
+        // Debug.Log("current lang: " + LocalizationSettings.SelectedLocale.Formatter);
+        // Debug.Log(stringTableCollection.GetTable(LocalizationSettings.SelectedLocale.Identifier));
+        // var table = LocalizationSettings.StringDatabase.GetTable(tableReference);
+        if (playerJSONData.hasSaveData())
+        {
+            gameObject.GetComponent<LocalizeStringEvent>().StringReference.SetReference(loadGameStrID.TableReference, loadGameStrID.TableEntryReference);
+        }
+        else
+        {
+            gameObject.GetComponent<LocalizeStringEvent>().StringReference.SetReference(startGameStrID.TableReference, startGameStrID.TableEntryReference);
+        }
     }
 }
